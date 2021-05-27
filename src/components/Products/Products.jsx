6e3e3e49870fd6506f32914d81296b102156f1a1
Toubic <toubic@gmail.com>
2021-05-27 04:00:06 +0200

    functionality for importing products from commerce.js

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,12 +4,7 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product'
 import useStyles from './styles'
 
-const products = [
-    {id: 1, name: 'Shoes', description: 'Running shoes', price: '500 sek', image: 'https://images.pexels.com/photos/2729899/pexels-photo-2729899.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'},
-    {id: 2, name: 'Macbook', description: 'Apple Macbook', price: '1000 sek', image: 'https://images.pexels.com/photos/5412266/pexels-photo-5412266.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'}
-];
-
-const Products = () => {
+const Products = ({ products }) => {
     const classes = useStyles();
     return(
         <main className={classes.content}>
