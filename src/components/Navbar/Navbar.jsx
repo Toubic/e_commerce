1c81c0529042873116b459e5a22ac08530009d51
Toubic <toubic@gmail.com>
2021-06-08 23:39:44 +0200

    + routing

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
 import { ShoppingCart, Shoppingcart } from '@material-ui/icons';
+import { Link } from 'react-router-dom';
 
 import logo from '../../assets/TwiCommerce.png'
 import useStyles from './styles'
@@ -12,13 +13,13 @@ const Navbar = ({ totalItemsInCart }) => {
         <>
             <AppBar position="fixed" className={classes.appBar} color="inherit">
                 <Toolbar>
-                    <Typography variant="h6" className={classes.title} color="inherit">
+                    <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
                         <img src={logo} alt="E-commerce" height="25px" className={classes.image} />
                         E-commerce
                     </Typography>
                     <div className={classes.grow} />
                     <div className={classes.button}>
-                        <IconButton aria-label="Show cart item" color="inherit">
+                        <IconButton component={Link} to="/cart" aria-label="Show cart item" color="inherit">
                             <Badge badgeContent={totalItemsInCart} color="secondary">
                                 <ShoppingCart />
                             </Badge>
