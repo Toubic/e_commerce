3ef3791afb7e04068c7c8f6b0d50c1585ca192a9
Toubic <toubic@gmail.com>
2021-06-10 23:50:24 +0200

    Functions for handling quantity in cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,7 +4,7 @@ import { Container, Typography, Button, Grid } from '@material-ui/core';
 import useStyles from './styles';
 import CartItem from './CartItem/CartItem';
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart, handleUpdateCartQuantity, handleRemoveFromCart, handleEmptyTheCart }) => {
     const classes = useStyles();
 
     const EmptyCart = () => (
@@ -16,14 +16,14 @@ const Cart = ({ cart }) => {
           <Grid container spacing={3}>
             {cart.line_items.map((item) => (
               <Grid item xs={12} sm={4} key={item.id}>
-                <CartItem item={item}/>
+                <CartItem item={item} onUpdateCartQuantity={handleUpdateCartQuantity} onRemoveFromCart={handleRemoveFromCart}/>
               </Grid>
             ))}
           </Grid>
           <div className={classes.cardDetails}>
             <Typography variant="h4">Subtotal: {cart.subtotal.formatted_with_symbol}</Typography>
             <div>
-              <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary">Empty cart</Button>
+              <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary" onClick={handleEmptyTheCart}>Empty cart</Button>
               <Button className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout</Button>
             </div>
           </div>
