0df9d2886e9fb81bcb1a78bdb4231f1b0ef39401
Toubic <toubic@gmail.com>
2021-06-21 19:10:42 +0200

    generate a checkout token

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
 import { useForm, FormProvider } from 'react-hook-form';
+
+import { commerce } from '../../lib/commerce';
 
 import FormInput from './FormInput';
 
 const AddressForm = () => {
+    const [shippingCountries, setShippingCountries] = useState([]);
+    const [shippingCountry, setShippingCountry] = useState('');
+    const [shippingSubdivisions, setShippingSubdivisions] = useState([]);
+    const [shippingSubdivision, setShippingSubdivision] = useState('');
+    const [shippingOptions, setShippingOptions] = useState([]);
+    const [shippingOption, setShippingOption] = useState('');
     const methods = useForm();
+
+    const fetchShippingCountries = async (checkoutTokenId) => {
+        const { countries } = await commerce.services.localeListShippingCountries(checkoutTokenId);
+
+        setShippingCountries(countries);
+    }
 
     return (
         <>
@@ -13,12 +27,30 @@ const AddressForm = () => {
             <FormProvider {... methods}>
                 <form onSubmit=''>
                     <Grid container spacing={3}>
-                    <FormInput required name="firstName" label="First name" />
-                    <FormInput required name="lastName" label="Last name" />
-                    <FormInput required name="address1" label="Address" />
-                    <FormInput required name="email" label="Email" />
-                    <FormInput required name="city" label="City" />
-                    <FormInput required name="postalCode" label="Postal code" />
+                        <FormInput required name="firstName" label="First name" />
+                        <FormInput required name="lastName" label="Last name" />
+                        <FormInput required name="address1" label="Address" />
+                        <FormInput required name="email" label="Email" />
+                        <FormInput required name="city" label="City" />
+                        <FormInput required name="postalCode" label="Postal code" />
+                        {/* <Grid item xs={12} sm={6}>
+                            <InputLabel>Shipping country</InputLabel>
+                            <Select value={} fullWidth onChange={}>
+                                <MenuItem value={} key={}>Select me</MenuItem>
+                            </Select>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <InputLabel>Shipping subdivision/InputLabel>
+                            <Select value={} fullWidth onChange={}>
+                                <MenuItem value={} key={}>Select me</MenuItem>
+                            </Select>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <InputLabel>Shipping options</InputLabel>
+                            <Select value={} fullWidth onChange={}>
+                                <MenuItem value={} key={}>Select me</MenuItem>
+                            </Select>
+                        </Grid> */}
                     </Grid>
                 </form>
             </FormProvider>
