e786ae687eae2a6002c1114dd0c115fdcd2aeead
Toubic <toubic@gmail.com>
2021-07-01 21:12:04 +0200

    Added margin-top to div for buttons in AddressForm

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -97,9 +97,8 @@ const AddressForm = ({ checkoutToken }) => {
                             </Select>
                         </Grid>
                     </Grid>
-                    <br />
-                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                    <   Button component={Link} variant="outlined" to="/cart">Back to Cart</Button>
+                    <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '10px' }}>
+                        <Button component={Link} variant="outlined" to="/cart">Back to Cart</Button>
                         <Button type="submit" variant="contained" color="primary">Next</Button>
                     </div>
                 </form>
