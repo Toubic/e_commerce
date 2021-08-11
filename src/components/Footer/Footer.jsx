9800544b844e6e9c6a72ebffee398f18feaf7e10
Toubic <toubic@gmail.com>
2021-08-11 15:23:33 +0200

    add link to github repo in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,6 +8,7 @@ const Footer = () => {
     return (
         <>
             <Typography variant="subtitle1" align="center" className={classes.typography}>Made by Toubic.</Typography>
+            <a href="https://github.com/Toubic/e_commerce"><Typography variant="subtitle1" align="center" className={classes.typography}>GITHUB</Typography></a>
         </>
     )
 }
